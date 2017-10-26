cd6aca7d7f3632b9e7e531392ebdf46f25cd456c
JonásMauricio <jonas.astudillo@usach.cl>
2017-10-26 11:00:30 -0300

    Foods search added.

--- app/src/main/java/com/example/jonsmauricio/eyesfood/data/api/EyesFoodApi.java
+++ app/src/main/java/com/example/jonsmauricio/eyesfood/data/api/EyesFoodApi.java
@@ -28,7 +28,8 @@ public interface EyesFoodApi {
     //Cambiar host por IP de tu PC para dispositivo real.
     // Esta es la ip de usach alumnos
     // public static final String BASE_URL = "http://158.170.214.219/api.eyesfood.cl/v1/";
-    String BASE_URL = "http://192.168.0.103/api.eyesfood.cl/v1/";
+    //String BASE_URL = "http://192.168.0.103/api.eyesfood.cl/v1/";
+    String BASE_URL = "https://eyesfood.000webhostapp.com/api.eyesfood.cl/v1/";
 
     //Esta petición tiene un parámetro del tipo @Body que es un LoginBody llamado loginBody
     @POST("users/login")
@@ -39,38 +40,27 @@ public interface EyesFoodApi {
 
     //Petición que retorna un alimento completo mediante su código de barras
     @GET("foods/{barcode}")
-    Call<Food> getFood(@Header("authorization") String token,
-                       @Path("barcode") String barcode);
+    Call<Food> getFood(@Path("barcode") String barcode);
 
     //Petición que retorna los ingredientes de un alimento
     @GET("foods/{barcode}/ingredients")
-    Call<List<Ingredient>> getIngredients(@Header("authorization") String token,
-                                          @Path("barcode") String barcode);
+    Call<List<Ingredient>> getIngredients(@Path("barcode") String barcode);
 
     //Petición que retorna los aditivos de un alimento
     @GET("foods/{barcode}/additives")
-    Call<List<Ingredient>> getAdditives(@Header("authorization") String token,
-                                          @Path("barcode") String barcode);
-
-    //Petición que retorna la información detallada de un aditivo
-    @GET("additives/{ECode}")
-    Call<Additive> getAdditive(@Header("authorization") String token,
-                                        @Path("ECode") String ECode);
+    Call<List<Ingredient>> getAdditives(@Path("barcode") String barcode);
 
     //Petición que retorna las recomendaciones de un alimento mediante su código de barras
     @GET("foods/{barcode}/recommendations")
-    Call<List<Recommendation>> getRecommendations(@Header("authorization") String token,
-                                        @Path("barcode") String barcode);
+    Call<List<Recommendation>> getRecommendations(@Path("barcode") String barcode);
 
     //Petición que consulta si el usuario tiene un alimento en su historial
     @GET("history/{userId}/{barcode}")
-    Call<ShortFood> isInHistory(@Header("authorization") String token,
-                                                  @Path("userId") String userId, @Path("barcode") String barcode);
+    Call<ShortFood> isInHistory(@Path("userId") String userId, @Path("barcode") String barcode);
 
     //Petición que retorna los alimentos del historial de usuario
     @GET("history/{userId}")
-    Call<List<ShortFood>> getFoodsInHistory(@Header("authorization") String token,
-                                @Path("userId") String userId);
+    Call<List<ShortFood>> getFoodsInHistory(@Path("userId") String userId);
 
     //Petición que inserta un alimento en el historial
     @POST("history")
@@ -83,6 +73,9 @@ public interface EyesFoodApi {
 
     //Petición que retorna los aditivos de un alimento con todos sus datos
     @GET("foods/{barcode}/additives/full")
-    Call<List<Additive>> getFullAdditives(@Header("authorization") String token,
-                                            @Path("barcode") String barcode);
+    Call<List<Additive>> getFullAdditives(@Path("barcode") String barcode);
+
+    //Petición que retorna alimentos desde una búsqueda
+    @GET("search/foods/{query}")
+    Call<List<SearchResult>> getFoodsQuery(@Path("query") String query);
 }
